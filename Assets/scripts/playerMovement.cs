966e9c406ba3affcbd5876b009c6ef8af1703093
Christian Sutter <christian-sutter@Christians-MacBook-Pro.local>
2020-10-14 16:05:04 +0200

    Initial commit

---
+++ Assets/scripts/playerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Rigidbody rb;
+    public int speed;
+    public GameObject player;
+
+    void Start()
+    {
+        rb = player.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      float moveHorizontal = Input.GetAxis("Horizontal");
+      float moveVertical = Input.GetAxis("Vertical");
+      Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+      rb.AddForce(movement*speed);
+    }
+}
